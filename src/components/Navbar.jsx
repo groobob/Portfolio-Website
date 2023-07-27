c20c4d054e3aa2d17070b890c90b5f1c90802c67
groobob <elitegem84@gmail.com>
2023-07-26 21:36:24 -0400

    Created navbar component skeleton

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,5 +3,14 @@ import { motion } from "framer-motion"
 
 export default function Navbar()
 {
-    
+    return (
+        <nav className="navbar">
+            <img src="logo" alt="logo" />
+            <h3 className="navbar--about">About</h3>
+            <h3 className="navbar--projects">Projects</h3>
+            <h3 className="navbar--commissions">Commissions</h3>
+            <h3 className="navbar--contacts">Contacts</h3>
+            <img src="menu" alt="menu" />
+        </nav>
+    )
 }
