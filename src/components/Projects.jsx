28b80afc7a85f2efb92e9451e20eda97a4dcba7e
groobob <elitegem84@gmail.com>
2023-07-26 21:30:49 -0400

    Created projects component skeleton

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,5 +3,13 @@ import { motion } from "framer-motion"
 
 export default function Projects()
 {
-    
+    return (
+        <section className="projects">
+            <h2 className="projects--header">My Stuff</h2>
+            <div className="projects--options"></div>
+            <div className="projects--container">
+                {"stuff"}
+            </div>
+        </section>
+    )
 }
