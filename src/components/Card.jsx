15457b890d7e026eb0c6059d73003cf5a10c19e9
groobob <elitegem84@gmail.com>
2023-07-28 16:31:53 -0400

    Added data to project

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { motion } from "framer-motion"
+
+export default function Card()
+{
+    return (
+        <div className="card">
+            
+        </div>
+    )
+}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { motion } from "framer-motion"
+import projectsData from "../projectsData"
 
 export default function Projects()
 {
