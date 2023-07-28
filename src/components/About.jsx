19aebc5e90a4cea77c2f077fa8d8eb182a864655
groobob <elitegem84@gmail.com>
2023-07-28 01:34:50 -0400

    More styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ export default function About()
             <div className="about--body">
                 <p className="about--text">I'm currently just a high school student interested in computers and the arts. 
                     More specifically, I mainly enjoy computer science, web development, digital art, and animation.
-                    In the future, I hope to make it into the University of Waterloo for computer science :).</p>
+                    In the future, I hope to make it into the University of Waterloo for computer science :)</p>
                 {/*<img src="image of me here" alt="personal image" /> */}
             </div>
         </section>
