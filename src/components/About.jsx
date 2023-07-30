3393cb7cf291600f31be5c50aa7028f210353b65
groobob <elitegem84@gmail.com>
2023-07-30 02:53:15 -0400

    Added contacts and more animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,13 +17,28 @@ export default function About()
                 >Hi there! I'm Richard Zhang</motion.h2>
             <div className="about--container">
                 <div className="about--body">
-                    <p className="about--text">I'm currently just a high school student interested in computers and the arts. 
-                        More specifically, game development in Unity, web development in React, digital art in Clip Studio Paint, and animation in Aseprite.
-                        In the future, I hope to make it into the University of Waterloo for computer science :)</p>
-                    <p className="about--text">I like video games, music, and animation. 
-                        If you would like to know some specifics on what I like, you can check it out <a className="about--link" href="https://docs.google.com/document/d/1mQjjlJGLDYcUABI0hCqKz8ewb_b8Yl7NF73MlYwbocU/edit?usp=sharing">here</a>! (little nerdy I know)</p>
-                    <p className="about--text">I hope to learn whatever I find cool and broaden my horizons when it comes to my skills. 
-                        For now, my ambitions lie with Blender and FL Studio for music composition.</p>
+                    <motion.p className="about--text"
+                        initial={{ opacity: 0, x: -100 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.4, ease: "easeOut" }}
+                        exit={{ opacity: 0 }}
+                    >I'm currently just a high school student interested in computers and the arts. 
+                    More specifically, game development in Unity, web development in React, digital art in Clip Studio Paint, and animation in Aseprite.
+                    In the future, I hope to make it into the University of Waterloo for computer science :)</motion.p>
+                    <motion.p className="about--text"
+                        initial={{ opacity: 0, x: -100 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.4, ease: "easeOut" }}
+                        exit={{ opacity: 0 }}
+                    >I like video games, music, and animation. 
+                    If you would like to know some specifics on what I like, you can check it out <a className="about--link" href="https://docs.google.com/document/d/1mQjjlJGLDYcUABI0hCqKz8ewb_b8Yl7NF73MlYwbocU/edit?usp=sharing">here</a>! (little nerdy I know)</motion.p>
+                    <motion.p className="about--text"
+                        initial={{ opacity: 0, x: -100 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.4, ease: "easeOut" }}
+                        exit={{ opacity: 0 }}
+                    >I hope to learn whatever I find cool and broaden my horizons when it comes to my skills. 
+                    For now, my ambitions lie with Blender and FL Studio for music composition.</motion.p>
                 </div>
                 <motion.img 
                 src="images/portrait.png"
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -6,16 +6,22 @@ export default function Contacts()
     return (
         <section className="contacts">
             <h2 className="contacts--header">Get in Contact!</h2>
-            <p className="contacts--text">If you would like reach out to me for any business inquiries or even would just like to chat, here are my socials. Maybe we could play around of Tetris ;)</p>
+            <p className="contacts--text">If you would like reach out to me for any business inquiries or if you just wanna talk, here are my contacts!</p>
             <div className="contacts--container">
                 <div className="contacts--box">
                     <h3 className="contacts--boxheader">Contacts</h3>
-                    <img src="some icons for stuff like linkedin or github" alt="github & linkedin pictures" />
-                    <p>links emails and stuff</p>
+                    <div className="contacts--iconcontainer">
+                        <a href="https://github.com/groobob" target="_blank" className="contacts--icons"><img src="images/icons/github.png"/></a>
+                        <a href="https://www.linkedin.com/in/richard-zhang-52705b27b/" target="_blank" className="contacts--icons"><img src="images/icons/linkedin.png"/></a>
+                    </div>
+                    <p className="contacts--caption">business email: richard.zhangonca@gmail.com</p>
                 </div>
                 <div className="contacts--box">
                     <h3 className="contacts--boxheader">Socials</h3>
-                    <img src="bunch of social media icons" alt="social media icons" />
+                    <div className="contacts--iconcontainer">
+                    <a href="https://www.instagram.com/groobaer/" target="_blank" className="contacts--icons"><img className="contacts--icons"src="images/icons/instagram.png"/></a>
+                    </div>
+                    <p className="contacts--caption">discord: groobr</p>
                 </div>
             </div>
         </section>
