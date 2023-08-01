88327cead90d8c79924aa3dc58f0858b778c6f58
groobob <elitegem84@gmail.com>
2023-07-31 22:19:34 -0400

    Added gradation

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -10,18 +10,21 @@ export default function Contacts()
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 5, delay: 6, ease: "easeOut" }}
+            viewport={{ once: true }}
         >
             <motion.h2 
-            className="contacts--header"
-            initial={{ opacity: 0}}
-            whileInView={{ opacity: 1}}
-            transition={{ duration: 1, delay: 0.3, ease: "easeOut" }}
+                className="contacts--header"
+                initial={{ opacity: 0}}
+                whileInView={{ opacity: 1}}
+                transition={{ duration: 1, delay: 0.3, ease: "easeOut" }}
+                viewport={{ once: true }}
             >Get in Contact!</motion.h2>
             <motion.p 
-            className="contacts--text"
-            initial={{ opacity: 0}}
-            whileInView={{ opacity: 1}}
-            transition={{ duration: 1, delay: 0.7, ease: "easeOut" }}
+                className="contacts--text"
+                initial={{ opacity: 0}}
+                whileInView={{ opacity: 1}}
+                transition={{ duration: 1, delay: 0.7, ease: "easeOut" }}
+                viewport={{ once: true }}
             >If you would like reach out to me for any business inquiries or if you just wanna talk, here are my contacts!</motion.p>
             <div className="contacts--container">
                 <div className="contacts--box">
@@ -30,6 +33,7 @@ export default function Contacts()
                         initial={{ opacity: 0, y: -50 }}
                         whileInView={{ opacity: 1, y: 0 }}
                         transition={{ duration: 0.5, delay: 1.5, ease: "easeOut" }}
+                        viewport={{ once: true }}
                     >Contacts</motion.h3>
                     <div className="contacts--iconcontainer">
                         <motion.a 
@@ -39,6 +43,7 @@ export default function Contacts()
                             initial={{ opacity: 0, y: -50 }}
                             whileInView={{ opacity: 1, y: 0 }}
                             transition={{ duration: 0.3, delay: 2.2, ease: "easeOut" }}
+                            viewport={{ once: true }}
                         ><img src="images/icons/github.png"/></motion.a>
                         <motion.a 
                             href="https://www.linkedin.com/in/richard-zhang-52705b27b/" 
@@ -47,6 +52,7 @@ export default function Contacts()
                             initial={{ opacity: 0, y: -50 }}
                             whileInView={{ opacity: 1, y: 0 }}
                             transition={{ duration: 0.3, delay: 2.2, ease: "easeOut" }}
+                            viewport={{ once: true }}
                         ><img src="images/icons/linkedin.png"/></motion.a>
                     </div>
                     <motion.p 
@@ -54,6 +60,7 @@ export default function Contacts()
                         initial={{ opacity: 0, y: -50 }}
                         whileInView={{ opacity: 1, y: 0 }}
                         transition={{ duration: 0.3, delay: 2.4, ease: "easeOut" }}
+                        viewport={{ once: true }}
                     >business email: richard.zhangonca@gmail.com</motion.p>
                 </div>
                 <div className="contacts--box">
@@ -61,7 +68,9 @@ export default function Contacts()
                         className="contacts--boxheader"
                         initial={{ opacity: 0, y: -50 }}
                         whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.5, delay: 1.5, ease: "easeOut" }}>Socials</motion.h3>
+                        transition={{ duration: 0.5, delay: 1.5, ease: "easeOut" }}
+                        viewport={{ once: true }}
+                    >Socials</motion.h3>
                     <div className="contacts--iconcontainer">
                     <motion.a 
                         href="https://www.instagram.com/groobaer/" 
@@ -70,13 +79,16 @@ export default function Contacts()
                         initial={{ opacity: 0, y: -50 }}
                         whileInView={{ opacity: 1, y: 0 }}
                         transition={{ duration: 0.3, delay: 2.2, ease: "easeOut" }}
+                        viewport={{ once: true }}
                     ><img className="contacts--icons"src="images/icons/instagram.png"/></motion.a>
                     </div>
                     <motion.p 
                         className="contacts--caption"
                         initial={{ opacity: 0, y: -50 }}
                         whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.3, delay: 2.4, ease: "easeOut" }}>discord: groobr</motion.p>
+                        transition={{ duration: 0.3, delay: 2.4, ease: "easeOut" }}
+                        viewport={{ once: true }}
+                    >discord: groobr</motion.p>
                 </div>
             </div>
         </motion.section>
